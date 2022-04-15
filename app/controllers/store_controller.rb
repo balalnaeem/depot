2ba5e9a2a2668ec889e4e4034a243e0b3a23eb04
Balal Naeem <balalnaeem@gmail.com>
2022-04-15 12:44:22 +0100

    Create cart to items added

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
+    session[:counter] = 0 unless session[:counter]
     @products = Product.order(:title)
+    session[:counter] += 1
+    @counter = session[:counter]
   end
 end
